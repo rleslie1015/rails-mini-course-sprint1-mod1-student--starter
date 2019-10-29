3d6657c160755a5402f82489271ef58b2fadcd88
leslie <rleslie1015@gmail.com>
2019-10-29 14:46:16 -0500

    practice with hashes

---
+++ candidates.rb
@@ -0,0 +1,30 @@
+# Create 3 different hashes based on the table of data above and assign them to variables `jane`, `sara`, and `jason` (use symbol keys and no hashrockets).
+jane = {}
+jane[first_name] = "Jane"
+jane[last_name] = "Doe"
+jane[level] = :senior_level
+jane[age] = 30
+jane[occupation] = "Software Developer"
+jane[employed] = true
+jane[current_salary] = 125,000.00
+jane[languages] = ["Ruby", "JavaScript", "Go"]
+
+sara = {}
+sara[first_name] = "Sara"
+sara[last_name] = "Smith"
+sara[level] = :mid_level
+sara[age] = 26
+sara[occupation] = "Software Developer"
+sara[employed] = true
+sara[current_salary] = 80,000.00
+sara[languages] = ["Ruby", "Python"]
+
+jason = {}
+jason[first_name] = "Jason"
+jason[last_name] = "Gray"
+jason[level] = :entry_level
+jason[age] = 23
+jason[occupation] = "Student"
+jason[employed] = false
+jason[current_salary] = 0.00
+jason[languages] = ["C++", "Python"]
